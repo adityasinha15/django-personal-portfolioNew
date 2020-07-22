b49314275aadb00e0876a00cc4db42706ca723cc
Aditya Sinha <adityasinha7927@gmail.com>
2020-07-22 19:20:09 +0530

    added S3

--- portfolioNew/settings.py
+++ portfolioNew/settings.py
@@ -39,7 +39,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',
+    'storages',
 ]
+
+
+
+AWS_STORAGE_BUCKET_NAME = 'django-personal-portfolio'
+AWS_S3_REGION_NAME = 'ap-south-1'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = 'AKIAJQEPNLAG5CI4YXUA'
+AWS_SECRET_ACCESS_KEY = 'xB263eovoJqE/0lRiF3pL5r+Dx93xqgcdICMVEHr'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL = None
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
