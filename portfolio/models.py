ff28cf80512018a4c01d76716de85a2344f62e90
Aditya Sinha <adityasinha7927@gmail.com>
2020-07-28 12:57:48 +0530

    models changed for storing urls of image instead of image

--- portfolio/models.py
+++ portfolio/models.py
@@ -18,21 +18,21 @@ class Project(models.Model):
     desc = models.CharField(max_length=1000)
     technologies = models.CharField(max_length=100)
     url = models.URLField(blank=True)
-    cover_image = models.ImageField(upload_to='portfolio/image/', blank=True)
+    cover_image = models.CharField(max_length=10000, blank=True)
     github = models.URLField(blank=True)
     def __str__(self):
         return self.title
 
 class Image(models.Model):
     project = models.OneToOneField(Project, on_delete=models.CASCADE)
-    img1 = models.ImageField(upload_to='portfolio/image/', blank=True)
-    img2 = models.ImageField(upload_to='portfolio/image/', blank=True)
-    img3 = models.ImageField(upload_to='portfolio/image/', blank=True)
-    img4 = models.ImageField(upload_to='portfolio/image/', blank=True)
-    img5 = models.ImageField(upload_to='portfolio/image/', blank=True)
-    img6 = models.ImageField(upload_to='portfolio/image/', blank=True)
-    img7 = models.ImageField(upload_to='portfolio/image/', blank=True)
-
+    img1 =  models.CharField(max_length=10000, blank=True)
+    img2 =  models.CharField(max_length=10000, blank=True)
+    img3 =  models.CharField(max_length=10000, blank=True)
+    img4 =  models.CharField(max_length=10000, blank=True)
+    img5 =  models.CharField(max_length=10000, blank=True)
+    img6 =  models.CharField(max_length=10000, blank=True)
+    img7 =  models.CharField(max_length=10000, blank=True)
+    img8 =  models.CharField(max_length=10000, blank=True)
 
 class Certification(models.Model):
     id = models.AutoField(primary_key=True)
@@ -46,7 +46,7 @@ class Profile(models.Model):
     fname = models.CharField(max_length=20)
     lname = models.CharField(max_length=20)
     dob = models.DateField()
-    resume = models.FileField(upload_to='portfolio/resume', blank=True)
+    resume = models.CharField(max_length=10000, blank=True)
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
